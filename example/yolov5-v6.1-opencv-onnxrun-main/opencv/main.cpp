1f67130e9e6d4100676a2a3689c2059f3f4f1b58
Wenrui Yu <56495635+Wenrui-Yu@users.noreply.github.com>
2025-05-28 09:27:36 +0200

    Update main.cpp

--- example/yolov5-v6.1-opencv-onnxrun-main/opencv/main.cpp
+++ example/yolov5-v6.1-opencv-onnxrun-main/opencv/main.cpp
@@ -58,8 +58,13 @@ YOLO::YOLO(Net_config config)
 
 	this->net = readNet(config.modelpath);
 
-	this->net.setPreferableBackend(cv::dnn::DNN_BACKEND_OPENCV);
-	this->net.setPreferableTarget(cv::dnn::DNN_TARGET_CPU);
+	//this->net.setPreferableBackend(cv::dnn::DNN_BACKEND_OPENCV);
+	//this->net.setPreferableTarget(cv::dnn::DNN_TARGET_CPU);
+
+        this->net.setPreferableBackend(DNN_BACKEND_CUDA);
+        this->net.setPreferableTarget(DNN_TARGET_CUDA);
+
+	
 	// //std::cout << cv::getBuildInformation() << std::endl;
 	// try {
     //     this->net.setPreferableBackend(cv::dnn::DNN_BACKEND_CUDA);
